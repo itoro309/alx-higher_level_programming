6b2ba2ed0dac6aad85f1b5c67887922040f13c61
itoro309 <louis4live@gmail.com>
2023-03-07 14:57:40 -0800

    slep

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if i == 99:
+        print("99")
+    else:
+        print("{0:0=2d}, ".format(i), end='')
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
