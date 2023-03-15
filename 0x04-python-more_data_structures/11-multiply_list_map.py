d1d00dc10f9776f5a3188bab11b882768eca9e79
itoro309 <louis4live@gmail.com>
2023-03-15 15:53:40 -0700

    updatted

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if my_list:
-        sum_product = (sum(map((lambda tupla: tupla[0] * tupla[1]), my_list)))
-        sum_weight = (sum(map((lambda tupla: tupla[1]), my_list)))
-        return sum_product / sum_weight
-    return 0
-
+def multiply_list_map(my_list=[], number=0):
+    return(list(map((lambda value: value * number), my_list)))
